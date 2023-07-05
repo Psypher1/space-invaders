946056fcee9956ee88364ec88263817ab8eda81b
Dante <jamesmidzi@gmail.com>
2023-07-05 15:13:20 +0200

    feat: multiple obstacles on screen and position correctly

--- main.py
+++ main.py
@@ -13,17 +13,29 @@ class Game:
         self.shape = shape
         self.block_size = 6
         self.blocks = pygame.sprite.Group()
-        self.create_obstacle()
+        self.obstacle_amount = 4
+        self.obstacle_x_positions = [
+            num * (screen_width / self.obstacle_amount)
+            for num in range(self.obstacle_amount)
+        ]
+        # self.create_obstacle(40, 500)
+        self.create_multiple_obstacles(
+            *self.obstacle_x_positions, x_start=screen_width / 12, y_start=500
+        )  # use * because position come in as list - unpack them
 
-    def create_obstacle(self):
+    def create_obstacle(self, x_start, y_start, offset_x):
         # enumarate to know where we are in the shape
         for row_index, row in enumerate(self.shape):
             for col_index, col in enumerate(row):
                 if col == "x":
-                    x = col_index * self.block_size
-                    y = row_index * self.block_size
+                    x = x_start + col_index * self.block_size + offset_x
+                    y = y_start + row_index * self.block_size
                     block = Block(self.block_size, (247, 79, 80), x, y)
                     self.blocks.add(block)
+
+    def create_multiple_obstacles(self, *offset, x_start, y_start):
+        for offset_x in offset:
+            self.create_obstacle(x_start, y_start, offset_x)
 
     def run(self):
         self.player.update()
